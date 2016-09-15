3ec3812f05740ecf9e60b216767fb6f998487be8
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2016-09-15 10:31:27 +0900

    Use OS's ca-bundle

--- logentries/recipes/default.rb
+++ logentries/recipes/default.rb
@@ -7,13 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 
-cookbook_file '/etc/ssl/certs/logentries.all.crt' do
-  source 'logentries.all.crt'
-  owner 'root'
-  group 'root'
-  mode '0755'
-end
-
 logentries = node[:deploy].map{ |e| e[1][:logentries] }.compact.first
 
 template '/etc/rsyslog.d/logentries.conf' do
